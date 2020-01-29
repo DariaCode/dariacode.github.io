0ae4bf9ab74854eb8d96ffd69ca4ac9b1adfdd70
Daria Code <vodadm@bk.ru>
2020-01-29 12:59:01 -0500

    test scroll navbar

---
+++ js/navbar.js
@@ -0,0 +1,27 @@
+/* ----------------------------------------------------
+file for www.dariacode.dev
+
+Updated: 01/29/2020
+Author: Daria Vodzinskaia
+Website: www.dariacode.dev
+-------------------------------------------------------  */
+
+
+let mainNavLinks = document.querySelectorAll("nav div a");
+let mainSection = document.querySelectorAll("section");
+let lastId;
+let cur = [];
+
+window.addEventListener("scroll", event =>{
+    let fromTop = window.scrollY;
+
+    mainNavLinks.forEach(link => {
+        let section = document.querySelector(link.hash);
+        
+        if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
+            link.classList.add("current");
+        } else {
+            link.classList.remove("current");
+        }
+    });
+});
